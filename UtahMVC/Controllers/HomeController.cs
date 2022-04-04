ae21666abbbd4af4c8f39bd1245e37dc7dc6c988
parkerkschmidt <parkerschmidt05@gmail.com>
2022-04-04 16:35:38 -0600

    idek anymore

--- UtahMVC/Controllers/HomeController.cs
+++ UtahMVC/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using UtahMVC.Models;
+using UtahMVC.Models.ViewModels;
 
 namespace UtahMVC.Controllers
 {
@@ -25,9 +26,27 @@ namespace UtahMVC.Controllers
         }
 
 
-        public IActionResult Crashes()
+        public IActionResult Crashes(int pageNum = 1)
         {
-            return View();
+            int pageSize = 10;
+
+            // calculate how many books to show on each page and have page numbers to correspond 
+            var x = new CrashesViewModel
+            {
+                Crashes = context.Crashes
+                .Where(c => c.CITY == "FAUST")
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumCrashes = context.Crashes.Count(),
+                    CrashesPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
         }
 
         public IActionResult Details(int id)
--- UtahMVC/Startup.cs
+++ UtahMVC/Startup.cs
@@ -57,8 +57,13 @@ namespace UtahMVC
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    "Paging",
+                    "{pageNum}",
+                    new { Controller = "Home", action = "Crashes", pageNum = 1 });
             });
         }
     }
