7d72de25e163caf3ae795d282ad477799a2355e3
jespiritu98 <jespiritu98@gmail.com>
2022-04-06 10:13:10 -0600

    Added Cookie notificatoin

--- UtahMVC/Startup.cs
+++ UtahMVC/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -26,6 +27,14 @@ namespace UtahMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential 
+                // cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                // requires using Microsoft.AspNetCore.Http;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
             services.AddControllersWithViews();
 
             services.AddDbContext<CrashesDbContext>(options =>
@@ -48,6 +57,7 @@ namespace UtahMVC
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
